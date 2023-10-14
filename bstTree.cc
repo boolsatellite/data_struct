7a4dee2fbad4d427461eb242c4f735c5e96fd623
boolsatellite <2477818421@qq.com>
2023-10-14 21:58:21 +0800

    BST树-子树问题

--- bstTree.cc
+++ bstTree.cc
@@ -13,30 +13,30 @@
 #include "functional"
 
 
-template<typename T , typename Compare = std::less<T>>
+template<typename T, typename Compare = std::less<T>>
 class BSTree {
 public:
-    BSTree() :root_(nullptr) { }
+    BSTree() : root_(nullptr) {}
 
     //非递归插入操作
-    void n_insert(const T& val) {
-        if(root_ == nullptr) {
+    void n_insert(const T &val) {
+        if (root_ == nullptr) {
             root_ = new Node(val);
             return;
         } else {
-            Node* cur = root_;
-            Node* parent = nullptr;
+            Node *cur = root_;
+            Node *parent = nullptr;
             while (cur != nullptr) {
                 parent = cur;
-                if(comp_( val , cur->data_ )) {
+                if (comp_(val, cur->data_)) {
                     cur = cur->left_;
-                } else if(comp_( cur->data_ , val)) {
+                } else if (comp_(cur->data_, val)) {
                     cur = cur->right_;
                 } else {                    //数据重复时不做操作
                     return;
                 }
             }
-            if(comp_(parent->data_ , val)) {
+            if (comp_(parent->data_, val)) {
                 parent->right_ = new Node(val);
             } else {
                 parent->left_ = new Node(val);
@@ -45,8 +45,8 @@ public:
     }
 
     //递归插入
-    void insert(const T& val) {
-        insert(root_ , val);
+    void insert(const T &val) {
+        insert(root_, val);
     }
 
     // 非递归删除
@@ -55,25 +55,25 @@ public:
      * 3. 有两个孩子节点       ->  找待删除节点的前驱或者后继节点，用前驱或者后继节点将
      *                           待删除节点覆盖掉，然后删除前驱或者后继节点,从而转化为 情况 1 ，2
      */
-    void n_remove(const T& val) {
-        if(root_ == nullptr)
+    void n_remove(const T &val) {
+        if (root_ == nullptr)
             return;
-        Node* parent = nullptr;
-        Node* cur = root_;
-        while(cur != nullptr) {
+        Node *parent = nullptr;
+        Node *cur = root_;
+        while (cur != nullptr) {
             parent = cur;
-            if(comp_(val , cur->data_)) {
+            if (comp_(val, cur->data_)) {
                 cur = cur->right_;
-            } else if (comp_(cur->data_ , val)) {
+            } else if (comp_(cur->data_, val)) {
                 cur = cur->left_;
             } else {    //cur->data_ == val  找到待删除节点
                 break;
             }
         }
-        if(cur == nullptr) return;          //当待删除节点不存在时
-        if(cur->left_ && cur->right_) {     //情况3
+        if (cur == nullptr) return;          //当待删除节点不存在时
+        if (cur->left_ && cur->right_) {     //情况3
             parent = cur;
-            Node* pre = cur->left_;
+            Node *pre = cur->left_;
             while (pre->right_) {           //找前驱节点
                 parent = pre;
                 pre = pre->right_;
@@ -82,14 +82,13 @@ public:
             cur = pre;
         }
         //情况 1 2
-        Node* child = cur->left_;   //child指向cur有孩子的那一侧
-        if(child == nullptr)
+        Node *child = cur->left_;   //child指向cur有孩子的那一侧
+        if (child == nullptr)
             child = cur->right_;
-        if(parent == nullptr) {     //删除的节点是根节点
+        if (parent == nullptr) {     //删除的节点是根节点
             root_ = child;
             delete cur;
-        }
-        else {
+        } else {
             if (parent->left_ == cur) {
                 parent->left_ = child;
                 delete cur;
@@ -101,17 +100,17 @@ public:
     }
 
     //递归删除
-    void remove(const T& val) {
-        root_ = remove(root_ , val);
+    void remove(const T &val) {
+        root_ = remove(root_, val);
     }
 
 
     //非递归查询
-    bool n_query(const T& val) {
-        Node* cur = root_;
+    bool n_query(const T &val) {
+        Node *cur = root_;
         while (cur != nullptr) {
-            if(cur->data_ == val) return true;
-            else if (comp_(cur->data_ , val )) {
+            if (cur->data_ == val) return true;
+            else if (comp_(cur->data_, val)) {
                 cur = cur->right_;
             } else {
                 cur = cur->left_;
@@ -122,33 +121,33 @@ public:
 
 
     void preOrder() {                   //递归前序遍历接口
-        std::cout << "[递归]  前序遍历    " ;
+        std::cout << "[递归]  前序遍历    ";
         preOrder(root_);
         std::cout << std::endl;
     }
 
     void inOrder() {            //递归中序遍历接口
-        std:: cout << "[递归] 中序遍历" ;
+        std::cout << "[递归] 中序遍历";
         inOrder(root_);
         std::cout << std::endl;
     }
 
     void postOrder() {          //递归后序遍历接口
-        std::cout <<"[递归] 后序遍历";
+        std::cout << "[递归] 后序遍历";
         postOrder(root_);
         std::cout << std::endl;
     }
 
     void levelOrder() {         //递归层序遍历接口
-        std::cout <<"[递归]";
+        std::cout << "[递归]";
         int h = high();
-        for( int i = 0 ; i < h ; i++) {
-            levelOrder(root_ , i);
+        for (int i = 0; i < h; i++) {
+            levelOrder(root_, i);
         }
     }
 
-    bool query(const T& val) {
-        return nullptr == query(root_ , val);
+    bool query(const T &val) {
+        return nullptr == query(root_, val);
     }
 
     int high() {               //递归求层高接口
@@ -160,11 +159,11 @@ public:
     }
 
     void n_preOrder() {
-        if(root_ == nullptr) return;
-        std::cout << "[非递归] 前序遍历"<<"   ";
-        std::stack<Node*> s;
+        if (root_ == nullptr) return;
+        std::cout << "[非递归] 前序遍历" << "   ";
+        std::stack<Node *> s;
         s.push(root_);
-        while(!s.empty()) {
+        while (!s.empty()) {
             Node *top = s.top();
             s.pop();
             std::cout << top->data_ << " ";
@@ -176,20 +175,20 @@ public:
     }
 
     void n_inOrder() {         //非递归中序遍历
-        if(root_ == nullptr) return;
-        std::cout << "[非递归] 中序遍历"<<"   ";
-        std::stack<Node*> s;
-        Node* cur = root_;
-        while(cur != nullptr) {
+        if (root_ == nullptr) return;
+        std::cout << "[非递归] 中序遍历" << "   ";
+        std::stack<Node *> s;
+        Node *cur = root_;
+        while (cur != nullptr) {
             s.push(cur);
             cur = cur->left_;
         }
-        while(!s.empty()) {
-            Node* top = s.top();
+        while (!s.empty()) {
+            Node *top = s.top();
             s.pop();
-            std::cout << top->data_ <<" ";
+            std::cout << top->data_ << " ";
             cur = top->right_;
-            while(cur != nullptr) {
+            while (cur != nullptr) {
                 s.push(cur);
                 cur = cur->left_;
             }
@@ -198,198 +197,226 @@ public:
     }
 
     void n_postOrder() {            //非递归后序遍历
-        if(root_ == nullptr) return;
+        if (root_ == nullptr) return;
         std::cout << "[非递归] 后序遍历" << " ";
-        std::stack<Node*> s1 , s2;      //V R L
+        std::stack<Node *> s1, s2;      //V R L
         s1.push(root_);
-        while(!s1.empty()) {
-            Node* cur = s1.top();
+        while (!s1.empty()) {
+            Node *cur = s1.top();
             s1.pop();
             s2.push(cur);
-            if(cur->left_ != nullptr) {
+            if (cur->left_ != nullptr) {
                 s1.push(cur->left_);
             }
-            if(cur->right_ != nullptr) {
+            if (cur->right_ != nullptr) {
                 s1.push(cur->right_);
             }
         }
         while (!s2.empty()) {
-            Node* top = s2.top();
+            Node *top = s2.top();
             s2.pop();
-            std:: cout << top->data_ <<" ";
+            std::cout << top->data_ << " ";
         }
         std::cout << std::endl;
     }
 
-    void findValues(std::vector<T>& vec , int i , int j) {      //查找位于(i,j)之间的元素并存放在vec中 ,递归接口
-        if(root_ == nullptr) assert(0);
-        findValues(root_ , vec , i , j);
+    void findValues(std::vector<T> &vec, int i, int j) {      //查找位于(i,j)之间的元素并存放在vec中 ,递归接口
+        if (root_ == nullptr) assert(0);
+        findValues(root_, vec, i, j);
     }
 
-    bool isBSTtrr() {
-        Node* pre = nullptr;
-        return isBSTtrr(root_ , pre);
+    bool isBSTtrr() {                                       //判断二叉树是否为二叉树
+        Node *pre = nullptr;
+        return isBSTtrr(root_, pre);
     }
+
+    bool isChildTree(const BSTree<T> &bst) {                //子树问题
+        if (bst.root_ == nullptr) return true;
+        Node *cur = root_;
+        while (cur != nullptr) {
+            if (cur->data_ == bst.root_->data_) break;
+            else if (cur->data_ > bst.root_->data_) {
+                cur = cur->left_;
+            } else {
+                cur = cur->right_;
+            }
+            if (cur == nullptr) return false;
+            return isChildTree(cur, bst.root_);
+        }
+    }
+
 
 private:
     struct Node;
 
-    bool isBSTree(Node* node , Node* &pre) {          //在递归处理中主要判断使递归结束的条件
-        if(node == nullptr) return true;
-        if( isBSTtrr(node->left_ , pre) == false) return false;
+    bool isChildTree(Node *cur, Node *child) {          //判断子树递归
+        if (cur == nullptr && child == nullptr) return true;
+        if (cur == nullptr) return false;
+        if (child == nullptr) return true;
 
-        if(pre != nullptr) {
-            if(pre->data_ > node->data_) return false;
+        if(cur->data_ != child->data_) return false;
+
+        return isChildTree(cur->left_ , child->right_) && isChildTree(cur->right_ , child->right_);
+    }
+
+    bool isBSTree(Node *node, Node *&pre) {
+        if (node == nullptr) return true;               //在递归处理中主要判断使递归结束的条件
+        if (isBSTtrr(node->left_, pre) == false) return false;
+
+        if (pre != nullptr) {
+            if (pre->data_ > node->data_) return false;
         }
         pre = node;
 
-        if( isBSTtrr(node->right_ , pre) == false) return false;
+        if (isBSTtrr(node->right_, pre) == false) return false;
         return true;
     }
 
-    void findValues(Node* node , std::vector<T>& vec , int i , int j) {      //查找位于(i,j)之间的元素并存放在vec中 递归实现
+    void findValues(Node *node, std::vector<T> &vec, int i, int j) {      //查找位于(i,j)之间的元素并存放在vec中 递归实现
         //采用中序遍历为升序的思想
-        if(node == nullptr) return;
-        if(node->data_ > i) {
+        if (node == nullptr) return;
+        if (node->data_ > i) {
             findValues(node->left_, vec, i, j);
         }
-        if(node->data_ >= i &&  node->data_ <= j) {
+        if (node->data_ >= i && node->data_ <= j) {
             vec.push_back(node->data_);
         }
-        if(node->data_ < j) {
+        if (node->data_ < j) {
             findValues(node->right_, vec, i, j);
         }
     }
-    Node* insert(Node* node , const T& val) {   //递归插入
-        if(node == nullptr) {
+
+    Node *insert(Node *node, const T &val) {   //递归插入
+        if (node == nullptr) {
             return new Node(val);
         }
-        if(node->data_ > val) {
-            node->right_ = insert(node->right_ , val);
-        } else if(node->data_ < val){
-            node->left_ = insert(node->left_ , val);
+        if (node->data_ > val) {
+            node->right_ = insert(node->right_, val);
+        } else if (node->data_ < val) {
+            node->left_ = insert(node->left_, val);
         } else {
             return node;
         }
         return node;
     }
 
-    void preOrder(Node* node) {         //前序遍历递归
-        if(node == nullptr) return;
-        std::cout <<node->data_ <<" ";
+    void preOrder(Node *node) {         //前序遍历递归
+        if (node == nullptr) return;
+        std::cout << node->data_ << " ";
         preOrder(node->left_);
         preOrder(node->right_);
     }
 
-    void inOrder(Node* node) {          //中序遍历递归
+    void inOrder(Node *node) {          //中序遍历递归
         if (node == nullptr) return;
         inOrder(node->left_);
-        std::cout << node->data_ <<" ";
+        std::cout << node->data_ << " ";
         inOrder(node->right_);
     }
 
-    void postOrder(Node* node) {         //后序遍历递归
+    void postOrder(Node *node) {         //后序遍历递归
         if (node == nullptr) return;
         postOrder(node->left_);
         postOrder(node->right_);
-        std::cout << node->data_ <<" ";
+        std::cout << node->data_ << " ";
     }
 
-    int high(Node* node) {             //当前节点层高递归
+    int high(Node *node) {             //当前节点层高递归
         //当前节点的高度 = max(左子树高度 ， 右子树高度) + 1
-        if(node == nullptr) return 0;
+        if (node == nullptr) return 0;
         //return level(node->left_) > level(node->right_) ? level(node->left_) +1 : level(node->right_) + 1;
         int left = level(node->left_);
         int right = level(node->right_);
-        return left > right ? left+1 : right+1;
+        return left > right ? left + 1 : right + 1;
     }
 
-    int num(Node* node) {            //递归求节点个数
+    int num(Node *node) {            //递归求节点个数
         // 当前节点num = 左子树num + 右子树num + 1
-        if(node == nullptr) return 0;
+        if (node == nullptr) return 0;
         int left = num(node->left_);
         int right = num(node->right_);
-        return left > right ? left+1 : right+1;
+        return left > right ? left + 1 : right + 1;
     }
 
-    void levelOrder(Node* node , int high) {    //层序遍历递归
+    void levelOrder(Node *node, int high) {    //层序遍历递归
         /*相当于是根左右，在根判断是否在当前层，若在则打印并return
          * */
-        if( high == 0) {
-            std::cout << node->data_ <<" ";
+        if (high == 0) {
+            std::cout << node->data_ << " ";
             return;
         }
-        levelOrder(node->left_ , high-1);
-        levelOrder(node->right_ , high-1);
+        levelOrder(node->left_, high - 1);
+        levelOrder(node->right_, high - 1);
     }
 
-    Node* query(Node* node , const T& val) {
-        if(node == nullptr) return nullptr;
-        if(node->data_ == val)
+    Node *query(Node *node, const T &val) {
+        if (node == nullptr) return nullptr;
+        if (node->data_ == val)
             return node;
-        else if(node->data_ < val) {
-            return query(node->right_ , val);
+        else if (node->data_ < val) {
+            return query(node->right_, val);
         } else {
-            return query(node->left_ , val);
+            return query(node->left_, val);
         }
     }
 
-    Node* remove(Node* node , const T&val) {
-        if( node == nullptr) return nullptr;
-        if(node->data_ == val) {        //情况三
-            if(node->left_ && node->right_) {
-                Node* pre = node->left_;
-                while(pre->right_) {
+    Node *remove(Node *node, const T &val) {
+        if (node == nullptr) return nullptr;
+        if (node->data_ == val) {        //情况三
+            if (node->left_ && node->right_) {
+                Node *pre = node->left_;
+                while (pre->right_) {
                     pre = pre->right_;
                 }
                 node->data_ = pre->data_;
-                node->left_ = remove(node->left_ , pre->data_);
+                node->left_ = remove(node->left_, pre->data_);
             } else {
-                if(node->left_ != nullptr) {
-                    Node* t = node->left_;
-                    delete(node);
+                if (node->left_ != nullptr) {
+                    Node *t = node->left_;
+                    delete (node);
                     return t;
-                } else if(node->right_ != nullptr) {
-                    Node* t = node->right_;
-                    delete(node);
+                } else if (node->right_ != nullptr) {
+                    Node *t = node->right_;
+                    delete (node);
                     return t;
                 } else {
-                    delete(node);
+                    delete (node);
                     return nullptr;
                 }
             }
-        } else if(node->data_ > val) {
+        } else if (node->data_ > val) {
             node->left_ = remove(node->left_, val);
         } else {
-            node->right_ = remove(node->right_ , val);
+            node->right_ = remove(node->right_, val);
         }
         return node;
     }
 
     struct Node {       //节点结构体定义
-        Node(T data=T{} , Node* left = nullptr , Node* right= nullptr)
-            :data_(data) , left_(left) , right_(right) {}
+        Node(T data = T{}, Node *left = nullptr, Node *right = nullptr)
+                : data_(data), left_(left), right_(right) {}
+
         T data_;
-        Node* left_;
-        Node* right_;
+        Node *left_;
+        Node *right_;
     };
 
-    Node* root_;        //BST树根节点
+    Node *root_;        //BST树根节点
     Compare comp_;
 };
 
 int main() {
 
-    int arr[] = {58,24,67,0,34,62,69,5,41,64,78};
+    int arr[] = {58, 24, 67, 0, 34, 62, 69, 5, 41, 64, 78};
     BSTree<int> bsTree;
-    for(int &i : arr) {
+    for (int &i: arr) {
         bsTree.n_insert(i);
     }
 
     bsTree.inOrder();
     std::vector<int> vi;
-    bsTree.findValues(vi,10,60);
-    for (auto &i : vi) {
-        std::cout << i <<" ";
+    bsTree.findValues(vi, 10, 60);
+    for (auto &i: vi) {
+        std::cout << i << " ";
     }
 };
