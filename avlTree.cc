f3b756bc0ea9e208eb03231398047c1ddde13b74
boolsatellite <2477818421@qq.com>
2023-12-04 11:39:34 +0800

    AVL树插入

--- avlTree.cc
+++ avlTree.cc
@@ -1,24 +1,69 @@
 //
 // Created by satellite on 2023-11-23.
 //
+#include <cmath>
+#include <algorithm>
 
 
-template<typename T>
+//template<typename T>
 class AVLTree {
 
 public:
+    AVLTree() : root_(nullptr) {}
+
+    void insert(int val) {
+        root_ = insert(root_, val);     //由于旋转会导致root_可能变化,故每次插入都要更新
+    }
 
 
 private:
     struct Node {
-        Node(T data = T())
-                : data_(data), left_(nullptr), right_(nullptr) {}
+        Node(int data = int())
+                : data_(data), left_(nullptr), right_(nullptr), hight_(1) {}
 
-        T data_;
+        int data_;
         struct Node *left_;
         struct Node *right_;
         int hight_;
     };
+
+    Node *insert(Node *node, const int &val) {
+        if (node == nullptr) {      // 递归结束，找到插入的位置了
+            return new Node(val);
+        }
+
+        if (node->data_ > val) {
+            node->left_ = insert(node->left_, val);
+            // 添加1 在递归回溯时判断节点是否失衡  node的左子树太高 node失衡了
+            if (hight(node->left_) - hight(node->right_) > 1) {
+                if (hight(node->left_->left_) >= hight(node->left_->right_)) {
+                    // 节点失衡，由于左孩子的左子树太高
+                    node = rightRotate(node);
+                } else {
+                    // 节点失衡，由于左孩子的右子树太高
+                    node = leftBlance(node);
+                }
+            }
+        } else if (node->data_ < val) {
+            node->right_ = insert(node->right_, val);
+            // 添加2 在递归回溯时判断节点是否失衡  node的右子树太高 node失衡了
+            if (hight(node->right_) - hight(node->left_) > 1) {
+                if (hight(node->right_->right_) >= hight(node->right_->left_)) {
+                    // 节点失衡，由于右孩子的右子树太高
+                    node = leftRotate(node);
+                } else {
+                    // 节点失衡，由于右孩子的左子树太高
+                    node = rightBlance(node);
+                }
+            }
+        } else { ; // 找到相同节点了，不用再往下递归了，直接向上回溯
+        }
+
+        // 添加3 因为子树中增加了新的节点  在递归回溯时检测更新节点高度
+        node->hight_ = std::max(hight(node->left_), hight(node->right_)) + 1;
+
+        return node;
+    }
 
     int hight(Node *node) {             //返回节点高度值
         if (node == nullptr) {
@@ -35,40 +80,44 @@ private:
         child->right_ = node;
         //高度更新
         node->hight_ = [&]() -> int {
-            hight(node->left_) >= hight(node->right_) ?
-            hight(node->left_) : hight(node->right_);
-        };
+            return
+                    hight(node->left_) >= hight(node->right_) ?
+                    hight(node->left_) : hight(node->right_);
+        }() + 1;
         child->hight_ = [&]() -> int {
-            hight(child->left_) >= hight(child->right_) ?
-            hight(child->left_) : hight(child->right_);
-        };
+            return
+                    hight(child->left_) >= hight(child->right_) ?
+                    hight(child->left_) : hight(child->right_);
+        }() + 1;
         return child;
     }
 
     //左旋转
-    void leftRotate(Node *node) {
+    Node *leftRotate(Node *node) {
         Node *child = node->right_;
         node->right_ = child->left_;
         child->left_ = node;
         node->hight_ = [&]() -> int {
-            hight(node->left_) >= hight(node->right_) ?
-            hight(node->left_) : hight(node->right_);
-        };
+            return
+                    hight(node->left_) >= hight(node->right_) ?
+                    hight(node->left_) : hight(node->right_);
+        }() + 1;
         child->hight_ = [&]() -> int {
-            hight(child->left_) >= hight(child->right_) ?
-            hight(child->left_) : hight(child->right_);
-        };
+            return
+                    hight(child->left_) >= hight(child->right_) ?
+                    hight(child->left_) : hight(child->right_);
+        }() + 1;
         return child;
     }
 
     //右平衡
-    Node* rightBlance(Node* node) {
+    Node *rightBlance(Node *node) {
         node->right_ = rightRotate(node->right_);
         return leftRotate(node);
     }
 
     //左平衡
-    Node* leftBlance(Node* node) {
+    Node *leftBlance(Node *node) {
         node->left_ = leftRotate(node->left_);
         return rightRotate(node);
     }
@@ -76,3 +125,10 @@ private:
     Node *root_;
 };
 
+int main() {
+    AVLTree avlTree{};
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for (auto i: arr) {
+        avlTree.insert(i);
+    }
+}
