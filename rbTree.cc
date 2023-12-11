0e87ef1680ef85db5b5186b8b881f2763305170e
boolsatellite <2477818421@qq.com>
2023-12-11 23:20:58 +0800

    红黑树旋转

--- rbTree.cc
+++ rbTree.cc
@@ -20,4 +20,97 @@
  *
  */
 
+class RBtree {
+public:
+    enum class Color {BLACK, RED};
+
+    RBtree() : root_(nullptr) { }
+
+private:
+    struct Node;
+    Node* root_;
+
+    void leftRotate(Node* node) {                   //左旋转
+        Node* child = node->right_;
+        child->parent_ = node->parent_;
+        if(node->parent_ == nullptr) {  //node 是 root_
+            root_ = child;
+        } else {                        //node 不为 root_
+            if(node->parent_->left_ == node) {      //node 在其父节点的左侧
+                node->parent_->left_ = child;
+            } else {                                //node 在其父节点的右侧
+                node->parent_->right_ = child;
+            }
+        }
+        node->right_ = child->left_;
+        if(child->left_ != nullptr) {
+            child->left_->parent_ = node;
+        }
+        child->left_ = node;
+        node->parent_ = child;
+    }
+
+    void rightRotate(Node* node) {
+         Node* child = node->left_;
+         if(node->parent_ == nullptr) {         //node 是 root_
+             root_ = child;
+         } else {
+             if(node->parent_->left_ == node) {
+                 node->parent_->left_ = child;
+             } else {
+                 node->parent_->right_ = node;
+             }
+             child->parent_ = node->parent_;
+             node->left_= child->right_;
+             if(child->right_ != nullptr) {
+                 child->right_->parent_ = node;
+             }
+             child->right_ = node;
+             node->parent_ = child;
+
+         }
+    }
+
+
+    Color Color(Node* node) const {
+        return node == nullptr ?  Color::BLACK : Color::RED;
+    }
+
+    void setColor(Node* node , enum Color color) {
+        node->color_ = color;
+    }
+
+    Node* left(Node* node) {
+        return node->left_;
+    }
+
+    Node* right(Node* node) {
+        return node->right_;
+    }
+
+    Node* parent(Node* node) {
+        return node->parent_;
+    }
+
+
+    struct Node {
+        Node(int data = 0,
+             Node *parent = nullptr, Node *left = nullptr, Node *right = nullptr, enum Color color = Color::BLACK)
+                :data_(data), left_(left), right_(right), parent_(parent), color_(color) {}
+
+        int data_;
+        Node *left_;
+        Node *right_;
+        Node *parent_;
+        enum Color color_;
+    };
+};
+
+
+
+
+
+
+
+
 
